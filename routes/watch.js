165e93d47d6982eedaf20bcf7cff0f31e3d22d3c
Tanner Summers <Tgsummers@cpp.edu>
2017-01-23 18:33:21 -0800

    Initial commit

---
+++ routes/watch.js
@@ -0,0 +1,126 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var chokidar = require('chokidar');
+var watch = require('node-watch');
+var mime = require('mime');
+var s3 = require('../aws');
+
+var gaze = require('gaze');
+var bucket = "restfulapiexamplebucket";
+
+var WatchIO = require('watch.io'),
+    watcher = new WatchIO();
+var util = require("util");
+
+router.get('/', function(req, res, next)
+{
+    var watcher = chokidar.watch('../data', {
+        persistent: true
+    });
+
+    watcher
+        .on('add', function(path, stats) {
+
+            console.log('File' + path + 'has been added');
+            var type = mime.lookup(path);
+            addToBucket(path, type, bucket);
+
+            if (stats) console.log('File ' + path + 'changed size to ' + stats.size);
+
+
+        })
+        .on('change', function(path, stats) {
+            console.log('File' + path + 'has been changed');
+            var type = mime.lookup(path);
+            addToBucket(path, type, bucket);
+
+        })
+        .on('unlink', function(path, stats) {
+            console.log('File' + path + 'has been unlinked');
+            deleteFromBucket(path);
+          //  if (stats) console.log('File ' + path + 'changed size to ' + stats.size);
+        });
+
+        // .on('raw', function(event, path, details) {
+        //     //
+        //     // if(event == 'rename') {
+        //     //
+        //     //   //  console.log('rename!!'+ event + " " + path +  " " + details);
+        //     // }
+        //
+        //
+        //     console.log('Raw event info:', event, path, details);
+        // });
+
+   // watcher.close();
+
+    res.render('watch', { title: 'File Storage' });
+
+
+
+});
+
+
+function addToBucket(path, mime, bucket)
+{
+    var split_path = path.split("/");
+    console.log(split_path);
+    var file_name = split_path[split_path.length - 1];
+
+    console.log("FILRNAME: " + file_name);
+    console.log("MIME: " + mime);
+
+
+    fs.readFile(path, function (err, data)
+    {
+        params = {
+
+            Body: data,
+            Bucket: bucket,
+            Key: file_name,
+           // ContentType: mime,
+            ContentType:  "image/png",
+            ACL: 'public-read'
+        };
+
+        s3.putObject(params, function(err, data) {
+            if (err) {
+                console.log(err)
+            } else {
+               // fs.unlink(path);
+            }
+        });
+    });
+
+}
+
+
+function deleteFromBucket(path) {
+
+    var split_path = path.split("/");
+
+    var file_name = split_path[split_path.length - 1];
+
+    console.log(split_path);
+    console.log(file_name);
+
+
+    fs.readFile(path, function(err,data)
+    {
+        params = {Bucket: bucket, Key: file_name};
+
+        s3.deleteObject(params, function(err, data)
+        {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("Deleted: " + bucket, data);
+            }
+        })
+    })
+}
+
+module.exports = router;
+
+
